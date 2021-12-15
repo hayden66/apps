5040236ce39c52dbc42ad77e4171a089433a82b8
Lucas Rodriguez <lucas.rodriguez@us.af.mil>
2021-12-14 22:04:01 -0600

    update flask-app

--- helm-deployments/flask-app/docker/app.py
+++ helm-deployments/flask-app/docker/app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/') 
 def index():
-    return 'This is version 0.1.1!'
+    return 'This is version 0.1.2!'
 if __name__ == "__main__":
     serve(app, listen='*:8080')
