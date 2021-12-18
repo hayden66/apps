9ea5b7cbd1a11256eba23db657a09d0a4f00079a
Lucas Rodriguez <lucasrod96@yahoo.com>
2021-12-18 13:48:27 -0600

    update namespace scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 kubectl create namespace argocd && \
-kubectl create namespace argo-rollouts && \
 kubectl create namespace ingress-nginx && \
-kubectl create namespace tekton-pipelines && \
 kubectl create namespace temp
--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 kubectl delete namespace app-manager && \
-kubectl delete namespace argo-rollouts && \
 kubectl delete namespace ingress-nginx && \
-kubectl delete namespace tekton-pipelines && \
 kubectl delete namespace temp
